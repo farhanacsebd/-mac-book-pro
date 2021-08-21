2605c00fb3acf3c52c0094e66b091257cbed1ac8
farhanacsebd <farhana.cse.bd@gmail.com>
2021-08-21 19:09:46 +0600

    comment done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// declear variable name for each ID element
 const button1 = document.getElementById('first');
 const button2 = document.getElementById('second');
 const button3 = document.getElementById('third');
@@ -13,7 +14,7 @@ const total = document.getElementById('total');
 const finalCost = document.getElementById('finalCost');
 let mainfinal;
 
-
+// update calculations
 function updateData() {
     const momoryPrice = Number(memoryCost.innerText);
     const mainPrice = Number(mainCost.innerText);
@@ -25,6 +26,8 @@ function updateData() {
     mainfinal = Number(finalCost.innerText);
 }
 
+
+// using onclick function for promo code
 function apply() {
     updateData();
     const promoField = document.getElementById("promoField").value;
@@ -35,36 +38,48 @@ function apply() {
 
 }
 
+
+// button1 event for 8GB memory
 button1.addEventListener('click', function () {
     memoryCost.innerText = "0";
 
     updateData();
 })
+
+// button2 event for 16GB memory
 button2.addEventListener('click', function () {
     memoryCost.innerText = "180";
 
     updateData();
 })
 
+// button3 event for 256GB ssd 
 button3.addEventListener('click', function () {
     storageCost.innerText = "0";
 
     updateData();
 })
+
+// button4 event for 512GB ssd 
 button4.addEventListener('click', function () {
     storageCost.innerText = "100";
 
     updateData();
 })
 
+// button5 event for 1TB ssd 
 button5.addEventListener('click', function () {
     storageCost.innerText = "180";
     updateData();
 })
+
+// button6 event for free delivery
 button6.addEventListener('click', function () {
     delivaryCost.innerText = "0";
     updateData();
 })
+
+// button7 event for paid delivery
 button7.addEventListener('click', function () {
     delivaryCost.innerText = "20";
     updateData();
